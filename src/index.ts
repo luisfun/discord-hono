4cfc1d79e22e5ba7ac440d3692b1f40928b18b4f
Luis <101060519+LuisFun@users.noreply.github.com>
2024-02-11 11:50:15 +0900

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type { ApplicationCommand, CommandHandler, Commands, ScheduledHandler } from './types'
 export type { Context, WaitUntilHandler } from './context'
-export type { RegisterArg } from './register'
 export { DiscordHono } from './discord-hono'
+export { Command, CommandOption } from "./command"
 export { register } from './register'
 export * from './api-wrapper/channel-message'
