699bb28a61cb9d3e097664052222844e01f827fe
Luis <101060519+LuisFun@users.noreply.github.com>
2024-05-04 04:41:16 +0900

    fix: deprecated postMessage

--- src/rest-api/post-message.ts
+++ src/rest-api/post-message.ts
@@ -2,6 +2,7 @@ import type { CustomCallbackData, FileData } from '../types'
 import { addToken, apiUrl, errorDev, fetch429Retry, formData } from '../utils'
 
 /**
+ * @deprecated
  * [Create Message](https://discord.com/developers/docs/resources/channel#create-message)
  * @param data [Data Structure](https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-interaction-callback-data-structure)
  * @param file FileData: { blob: Blob, name: string }
