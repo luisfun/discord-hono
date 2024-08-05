0c1a7ba4af0a42a4ada29c4777879842126a8fa2
Luis <101060519+LuisFun@users.noreply.github.com>
2024-08-05 18:13:56 +0900

    fix: Rest

--- src/rest-api/rest.ts
+++ src/rest-api/rest.ts
@@ -1,10 +1,13 @@
 import type {
+  RESTGetAPIChannelMessageReactionUsersQuery,
   RESTGetAPIChannelMessageReactionUsersResult,
   RESTGetAPIChannelMessageResult,
+  RESTGetAPIChannelMessagesQuery,
   RESTGetAPIChannelMessagesResult,
   RESTGetAPIChannelResult,
   RESTPatchAPIChannelMessageJSONBody,
   RESTPostAPIChannelMessageJSONBody,
+  RESTPostAPIChannelMessagesBulkDeleteJSONBody,
 } from 'discord-api-types/v10'
 import type { FileData } from '../types'
 import { addToken, apiUrl, errorDev, fetch429Retry, formData } from '../utils'
@@ -15,13 +18,23 @@ type GetPath =
   | '/channels/{channel.id}/messages/{message.id}'
   | '/channels/{channel.id}/messages/{message.id}/reactions/{emoji}'
 
-type GetResult<P extends GetPath> = P extends '/channels/{channel.id}/messages'
-  ? RESTGetAPIChannelMessagesResult
-  : P extends '/channels/{channel.id}/messages/{message.id}'
-    ? RESTGetAPIChannelMessageResult
-    : P extends '/channels/{channel.id}/messages/{message.id}/reactions/{emoji}'
-      ? RESTGetAPIChannelMessageReactionUsersResult
+type GetQuery<P extends GetPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages'
+    ? RESTGetAPIChannelMessagesQuery
+    : P extends '/channels/{channel.id}/messages/{message.id}'
+      ? RESTGetAPIChannelMessageReactionUsersQuery
       : undefined
+
+type GetResult<P extends GetPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages'
+    ? RESTGetAPIChannelMessagesResult
+    : P extends '/channels/{channel.id}/messages/{message.id}'
+      ? RESTGetAPIChannelMessageResult
+      : P extends '/channels/{channel.id}/messages/{message.id}/reactions/{emoji}'
+        ? RESTGetAPIChannelMessageReactionUsersResult
+        : undefined
 
 type PutPath =
   // Messages https://discord.com/developers/docs/resources/message
@@ -33,9 +46,29 @@ type PostPath =
   | '/channels/{channel.id}/messages/{message.id}/crosspost'
   | '/channels/{channel.id}/messages/bulk-delete'
 
+type PostData<P extends PostPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages'
+    ? RESTPostAPIChannelMessageJSONBody
+    : P extends '/channels/{channel.id}/messages/bulk-delete'
+      ? RESTPostAPIChannelMessagesBulkDeleteJSONBody
+      : undefined
+
+type PostFile<P extends PostPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages' ? FileData : undefined
+
 type PatchPath =
   // Messages https://discord.com/developers/docs/resources/message
   '/channels/{channel.id}/messages/{message.id}'
+
+type PatchData<P extends PatchPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages/{message.id}' ? RESTPatchAPIChannelMessageJSONBody : undefined
+
+type PatchFile<P extends PatchPath> =
+  // Messages https://discord.com/developers/docs/resources/message
+  P extends '/channels/{channel.id}/messages/{message.id}' ? FileData : undefined
 
 type DeletePath =
   // Messages https://discord.com/developers/docs/resources/message
@@ -64,67 +97,86 @@ type Variables<P extends GetPath | PutPath | PostPath | PatchPath | DeletePath> 
 
 export class Rest {
   #fetch
-  constructor(token: string, retry = 0) {
+  /**
+   * [Docs](https://discord-hono.luis.fun/rest-api/rest/)
+   *
+   * Supports: [Messages](https://discord.com/developers/docs/resources/message)
+   * @param {string} token
+   * @param {number} [retry=0] Number of retries when 429 etc.
+   */
+  constructor(token: string | undefined, retry = 0) {
     this.#fetch = (
       path: string,
       variables: string[],
       method: 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE',
-      body?: BodyInit,
+      body?: FormData | string,
     ) => {
+      if (!token) throw errorDev('DISCORD_TOKEN')
       const url =
         apiUrl +
         path
           .replace(' ', '')
           // Decompose path into array format
-          .split('{')
-          .flatMap(str => str.split('}'))
+          .split(/[{}]/)
           // Replace the contents of {} in the path
-          .map((str, i) => {
-            if (i % 2 === 0) return str
-            return variables[Math.trunc(i / 2)]
-          })
+          .map((str, i) => (i % 2 ? variables[~~(i / 2)] : str))
           .join('')
       const init = {
         method,
         body,
-        headers: body
-          ? { 'content-type': body instanceof FormData ? 'multipart/form-data' : 'application/json' }
-          : undefined,
+        headers: typeof body === 'string' ? { 'content-type': 'application/json' } : undefined,
       }
       return fetch429Retry(url, addToken(token, init), retry)
     }
   }
-  get = async <P extends GetPath>(path: P, variables: Variables<P>) => {
-    const response = await this.#fetch(path, variables, 'GET')
+  /**
+   * @param {string} path Official document path
+   * @param {string[]} variables Variable part of official document path
+   * @param query
+   * @returns {Promise<{response: Response, result: any}>}
+   */
+  get = async <P extends GetPath>(path: P, variables: Variables<P>, query?: GetQuery<P>) => {
+    const response = await this.#fetch(path, variables, 'GET', JSON.stringify(query))
     return { response, result: (await response.json()) as GetResult<P> }
   }
+  /**
+   * @param {string} path Official document path
+   * @param {string[]} variables Variable part of official document path
+   * @returns {Promise<Response>}
+   */
   put = <P extends PutPath>(path: P, variables: Variables<P>) => this.#fetch(path, variables, 'PUT')
-  post = <P extends PostPath>(path: P, variables: Variables<P>, data: any, file?: FileData) => {
-    let body: BodyInit
-    switch (path) {
-      case '/channels/{channel.id}/messages':
-        body = formData(data, file)
-        break
-      default:
-        body = JSON.stringify(data)
-    }
+  /**
+   * @param {string} path Official document path
+   * @param {string[]} variables Variable part of official document path
+   * @param data
+   * @param {FileData} file
+   * @returns {Promise<Response>}
+   */
+  post = <P extends PostPath>(path: P, variables: Variables<P>, data: PostData<P>, file?: PostFile<P>) => {
+    // @ts-expect-error
+    const body = file ? formData(data, file) : JSON.stringify(data)
     return this.#fetch(path, variables, 'POST', body)
   }
-  patch = <P extends PatchPath>(path: P, variables: Variables<P>, data: any, file?: FileData) => {
-    let body: BodyInit
-    switch (path) {
-      case '/channels/{channel.id}/messages/{message.id}':
-        body = formData(data, file)
-        break
-      default:
-        body = JSON.stringify(data)
-    }
+  /**
+   * @param {string} path Official document path
+   * @param {string[]} variables Variable part of official document path
+   * @param data
+   * @param {FileData} file
+   * @returns {Promise<Response>}
+   */
+  patch = <P extends PatchPath>(path: P, variables: Variables<P>, data: PatchData<P>, file?: PatchFile<P>) => {
+    const body = file ? formData(data, file) : JSON.stringify(data)
     return this.#fetch(path, variables, 'PATCH', body)
   }
+  /**
+   * @param {string} path Official document path
+   * @param {string[]} variables Variable part of official document path
+   * @returns {Promise<Response>}
+   */
   delete = <P extends DeletePath>(path: P, variables: Variables<P>) => this.#fetch(path, variables, 'DELETE')
 }
 
-//const a = new Rest("").post("/channels/{channel.id}/messages", [""], "")
+//const a = await new Rest("token").post("/channels/{channel.id}/messages", ["channel.id"], "data")
 
 const mGet = { method: 'GET' } as const
 const mPut = { method: 'PUT' } as const
@@ -142,6 +194,7 @@ type ChannelMessageGetResult<M extends string | undefined> = M extends string
  * other: response
  */
 /**
+ * @deprecated
  * @param token required
  * @param retry Number of retries at rate limit. default: 0
  */
