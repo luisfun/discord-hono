c9cfb2432a25d5e26dff1313cc985a2b0be0aedb
Luis <101060519+LuisFun@users.noreply.github.com>
2025-01-10 05:45:27 +0900

    refactor: verify

--- src/verify.ts
+++ src/verify.ts
@@ -5,8 +5,9 @@
 import { errorOther } from './utils'
 
 const hex2bin = (hex: string) => {
-  const bin = new Uint8Array(Math.ceil(hex.length / 2))
-  for (let i = 0; i < bin.length; i++) bin[i] = Number.parseInt(hex.slice(i * 2, i * 2 + 2), 16)
+  const len = hex.length
+  const bin = new Uint8Array(len >> 1)
+  for (let i = 0; i < len; i += 2) bin[i >> 1] = Number.parseInt(hex.substring(i, i + 2), 16)
   return bin
 }
 
