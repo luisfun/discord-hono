64dd5a0a634a816a8aaf8cb730694ffd41318ca2
Luis <101060519+LuisFun@users.noreply.github.com>
2025-09-12 16:38:24 +0900

    wip: bench

---
+++ src/test-helpers/verify.ts
@@ -0,0 +1,7 @@
+
+export const testVerifyTrue = async (_body: string, _signature: string | null, _timestamp: string | null, _publicKey: string) => {
+  return await true
+}
+export const testVerifyFalse = async (_body: string, _signature: string | null, _timestamp: string | null, _publicKey: string) => {
+  return await false
+}
